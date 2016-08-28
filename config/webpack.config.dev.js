15335cce43a04d34fc4b6f4147bf0f54b1d16867
zackshackleton <zackshackleton@gmail.com>
2016-08-28 17:00:36 -0400

    Set up SASS bundling

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -96,6 +96,12 @@ module.exports = {
         loader: 'babel',
         query: require('./babel.dev')
       },
+
+      {
+        test: /\.sass$/,
+        include: paths.appSrc,
+        loader: 'style!css!sass'
+      },
       // "postcss" loader applies autoprefixer to our CSS.
       // "css" loader resolves paths in CSS and adds assets as dependencies.
       // "style" loader turns CSS into JS modules that inject <style> tags.
